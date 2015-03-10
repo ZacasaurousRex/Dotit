16f8baf45595aa74e85cfc68596b357e16eb81ac
ZacasaurousRex <riverzac000@hotmail.com>
2015-03-09 17:34:24 -0700

    Starting the menu

--- app/src/main/java/com/pullstartstudios/dotit/HomeScreen.java
+++ app/src/main/java/com/pullstartstudios/dotit/HomeScreen.java
@@ -2,38 +2,23 @@ package com.pullstartstudios.dotit;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 
-public class HomeScreen extends ActionBarActivity {
+public class HomeScreen extends Activity {
+
+    //  Buttons on the home screen
+    ImageButton speedRound, dontTouch, fasterAndFaster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
