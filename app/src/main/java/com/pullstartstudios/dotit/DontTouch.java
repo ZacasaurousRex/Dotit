8eae7777726c12d1d875188e6358143d07fd20f4
ZacasaurousRex <riverzac000@hotmail.com>
2015-03-26 15:52:15 -0700

    Fade out for start button

--- app/src/main/java/com/pullstartstudios/dotit/DontTouch.java
+++ app/src/main/java/com/pullstartstudios/dotit/DontTouch.java
@@ -11,6 +11,10 @@ package com.pullstartstudios.dotit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 
@@ -18,17 +22,28 @@ public class DontTouch extends Activity {
 
     TextView score;
     int counter = 0;
+    ImageButton startDot;
+    Animation fadeOut;
+
     //  Creates Don't Touch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dont_touch);
-
+        score = (TextView) findViewById(R.id.score_counter);
     }// End on create
 
-    public void init(){
-        score = (TextView) findViewById(R.id.score_counter);
-        //  When the dot is clicked
-        //score.setText(counter);
-    }
+    public void createStart() {
+        startDot = (ImageButton) findViewById(R.id.start_dot);
+        fadeOut = AnimationUtils.loadAnimation(this, R.anim.abc_fade_out);
+        startDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDot.startAnimation(fadeOut);
+                startDot.setVisibility(View.INVISIBLE);
+                startDot.setClickable(false);
+            }
+        });
+
+    }// End create start
 }
